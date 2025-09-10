8a3e117f8008841a0db183a4a9c5cc9ebf3a0be8
FPStrunk <kowa9516@googlemail.com>
2025-09-10 10:37:41 +0200

    G: Player can now collide with asteroids leading to game over

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
+import sys
 from constants import *
 from player import Player
-from asteroidfield import *
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    clock =pygame.time.Clock()
-    dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     Asteroid_Field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +30,10 @@ def main():
         for item in drawable:
             item.draw(screen)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.col_check(player):
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
